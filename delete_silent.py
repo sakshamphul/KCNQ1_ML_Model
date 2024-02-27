38d98b3c567d3a48fd296648e885c5961156a63e
Saksham Phul <phulsaksham2@gmail.com>
2024-02-27 11:35:18 +0530

    Update delete_silent.py

--- delete_silent.py
+++ delete_silent.py
@@ -16,10 +16,7 @@ def writing_file(filename,data):
     with open(filename,"w") as f:
         for p in range(0,len(data),1):
             k=data[p].split(",")
-#	    d1=[k[0],k[1],k[2],k[3],k[4],k[5],k[6]]
 	    d1=[k[0],k[1],k[2],k[3],k[4],k[5],k[6],k[7],k[8]]	
-#	    d1=[k[0],k[1],k[2],k[3],k[4]]
-#	    d1=[k[0],k[1],k[2]]	
             d1=(str(d1).replace("[",""))
             d1=(str(d1).replace("]",""))
             d1=(str(d1).replace("'",""))
@@ -27,7 +24,6 @@ def writing_file(filename,data):
             f.write("\n")
         f.close()    
         
-print("Saksham")    
 a1=reading_file("concat.txt")    
 #a1=reading_file("a1q1.mutations.combined155123.csv")
 writing_file("deleted_silent.txt",a1)
